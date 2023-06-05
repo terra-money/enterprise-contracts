4b5053e8f8551d289434e3cc7593c1c291ffd6b7
Aleksa Boskov <aleksa.boskov@gmail.com>
2023-06-05 21:34:12 +0200

    Add multisig membership contract

---
+++ contracts/multisig-membership/README.md
@@ -0,0 +1,9 @@
+# Multisig membership
+
+A contract for managing a multisig membership for an Enterprise DAO.
+Essentially a proxy to the multisig-membership library.
+
+Mainly serves to:
+- store multisig members' weights
+- provide an interface to modify multisig members
+- provide queries for user and total weights
---
+++ contracts/multisig-membership/src/contract.rs
@@ -0,0 +1,68 @@
+use common::cw::{Context, QueryContext};
+use cosmwasm_std::{
+    entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response,
+};
+use cw2::set_contract_version;
+use multisig_membership_api::error::MultisigMembershipResult;
+use multisig_membership_api::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use multisig_membership_impl::execute::{update_config, update_members};
+use multisig_membership_impl::query::{query_total_weight, query_user_weight};
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:multisig-membership";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> MultisigMembershipResult<Response> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let ctx = &mut Context { deps, env, info };
+
+    multisig_membership_impl::instantiate::instantiate(ctx, msg)?;
+
+    Ok(Response::new().add_attribute("action", "instantiate"))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> MultisigMembershipResult<Response> {
+    let ctx = &mut Context { deps, env, info };
+
+    match msg {
+        ExecuteMsg::UpdateMembers(msg) => update_members(ctx, msg),
+        ExecuteMsg::UpdateConfig(msg) => update_config(ctx, msg),
+    }
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn reply(_deps: DepsMut, _env: Env, _msg: Reply) -> MultisigMembershipResult<Response> {
+    Ok(Response::new())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> MultisigMembershipResult<Binary> {
+    let qctx = QueryContext { deps, env };
+
+    let response = match msg {
+        QueryMsg::UserWeight(params) => to_binary(&query_user_weight(&qctx, params)?)?,
+        QueryMsg::TotalWeight(params) => to_binary(&query_total_weight(&qctx, params)?)?,
+    };
+
+    Ok(response)
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> MultisigMembershipResult<Response> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    Ok(Response::new().add_attribute("action", "migrate"))
+}
