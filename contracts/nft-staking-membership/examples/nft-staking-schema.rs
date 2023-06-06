72de04d862a015dfa79377c342e23958e9993d0c
Aleksa Boskov <aleksa.boskov@gmail.com>
2023-06-06 13:48:22 +0200

    Add NFT staking membership contract

---
+++ contracts/nft-staking-membership/examples/nft-staking-schema.rs
@@ -0,0 +1,26 @@
+use std::{env::current_dir, fs::create_dir_all};
+
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use enterprise_governance_api::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use poll_engine_api::api::{
+    PollResponse, PollStatusResponse, PollVoterResponse, PollVotersResponse, PollsResponse,
+    VoterResponse,
+};
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(MigrateMsg), &out_dir);
+    export_schema(&schema_for!(PollResponse), &out_dir);
+    export_schema(&schema_for!(PollsResponse), &out_dir);
+    export_schema(&schema_for!(PollStatusResponse), &out_dir);
+    export_schema(&schema_for!(PollVoterResponse), &out_dir);
+    export_schema(&schema_for!(PollVotersResponse), &out_dir);
+    export_schema(&schema_for!(VoterResponse), &out_dir);
+}
