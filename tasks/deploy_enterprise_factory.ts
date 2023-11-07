4f685dcfe2e291a80f5c8f68ce8d7261093c287d
Aleksa Boskov <aleksa.boskov@gmail.com>
2023-11-07 21:49:35 +0100

    Deploy the newest version of treasury contract to mainnet and testnet

--- tasks/deploy_enterprise_factory.ts
+++ tasks/deploy_enterprise_factory.ts
@@ -54,7 +54,7 @@ task(async ({network, deployer, executor, signer, refs}) => {
 
     // await deployEnterpriseFactory(refs, network, deployer, signer);
 
-    await deployNewEnterpriseVersion(refs, network, deployer, executor, 1, 3, 1);
+    await deployNewEnterpriseVersion(refs, network, deployer, executor, 1, 5, 0);
 
     // await instantiateDao(refs, network, executor);
 
