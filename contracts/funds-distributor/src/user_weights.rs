e2ce325366e4f296f176f10720a11874d81a28a3
Aleksa Boskov <aleksa.boskov@gmail.com>
2024-03-04 06:39:29 +0100

    Replace existing user weights update with trait-based one

--- contracts/funds-distributor/src/user_weights.rs
+++ contracts/funds-distributor/src/user_weights.rs
@@ -1,18 +1,20 @@
-use crate::cw20_distributions::{Cw20Distribution, CW20_DISTRIBUTIONS};
-use crate::eligibility::MINIMUM_ELIGIBLE_WEIGHT;
-use crate::native_distributions::{NativeDistribution, NATIVE_DISTRIBUTIONS};
-use crate::state::{ADMIN, CW20_GLOBAL_INDICES, EFFECTIVE_TOTAL_WEIGHT, NATIVE_GLOBAL_INDICES};
-use crate::{cw20_distributions, native_distributions};
+use crate::repository::asset_repository::{
+    asset_distribution_repository, AssetDistributionRepository,
+};
+use crate::repository::user_distribution_repository::{
+    user_distribution_repository_mut, UserDistributionRepositoryMut,
+};
+use crate::repository::weights_repository::{
+    weights_repository, weights_repository_mut, WeightsRepository, WeightsRepositoryMut,
+};
+use crate::state::{ADMIN, EFFECTIVE_TOTAL_WEIGHT};
 use common::cw::Context;
-use cosmwasm_std::Order::Ascending;
-use cosmwasm_std::{Addr, Decimal, DepsMut, Response, StdResult, Uint128};
-use cw20_distributions::update_user_cw20_distributions;
+use cosmwasm_std::{Addr, DepsMut, Response, Uint128};
 use cw_storage_plus::Map;
 use funds_distributor_api::api::{UpdateUserWeightsMsg, UserWeight};
 use funds_distributor_api::error::DistributorError::Unauthorized;
 use funds_distributor_api::error::{DistributorError, DistributorResult};
 use funds_distributor_api::response::execute_update_user_weights_response;
-use native_distributions::update_user_native_distributions;
 use DistributorError::DuplicateInitialWeight;
 
 pub const USER_WEIGHTS: Map<Addr, Uint128> = Map::new("user_weights");
@@ -71,61 +73,82 @@ pub fn update_user_weights(
         return Err(Unauthorized);
     }
 
-    update_user_weights_checked(ctx.deps.branch(), msg)
+    update_user_weights_checked(ctx.deps.branch(), msg)?;
+
+    Ok(execute_update_user_weights_response())
 }
 
-pub fn update_user_weights_checked(
+fn update_user_weights_checked(
     mut deps: DepsMut,
     msg: UpdateUserWeightsMsg,
-) -> DistributorResult<Response> {
-    let mut effective_total_weight = EFFECTIVE_TOTAL_WEIGHT.load(deps.storage)?;
-
-    let minimum_eligible_weight = MINIMUM_ELIGIBLE_WEIGHT.load(deps.storage)?;
+) -> DistributorResult<()> {
+    let mut total_weight = weights_repository(deps.as_ref()).get_total_weight()?;
 
     for user_weight_change in msg.new_user_weights {
         let user = deps.api.addr_validate(&user_weight_change.user)?;
 
-        let old_user_effective_weight =
-            EFFECTIVE_USER_WEIGHTS.may_load(deps.storage, user.clone())?;
+        let old_user_weight = weights_repository(deps.as_ref()).get_user_weight(user.clone())?;
 
-        match old_user_effective_weight {
+        match old_user_weight {
             None => {
                 // we have not encountered this user, so we need to ensure their distribution
                 // indices are set to current global indices
                 initialize_user_indices(deps.branch(), user.clone())?;
             }
-            Some(old_user_effective_weight) => {
+            Some(old_user_weight) => {
                 // the user already had their weight previously, so we use that weight
                 // to calculate how many rewards for each asset they've accrued since we last
                 // calculated their pending rewards
-                update_user_native_distributions(
-                    deps.branch(),
-                    user.clone(),
-                    old_user_effective_weight,
-                )?;
-                update_user_cw20_distributions(
-                    deps.branch(),
-                    user.clone(),
-                    old_user_effective_weight,
-                )?;
+                update_user_indices(deps.branch(), user.clone(), old_user_weight)?;
             }
         };
 
-        USER_WEIGHTS.save(deps.storage, user.clone(), &user_weight_change.weight)?;
+        let new_user_weight = weights_repository_mut(deps.branch())
+            .set_user_weight(user.clone(), user_weight_change.weight)?;
 
-        let effective_user_weight =
-            calculate_effective_weight(user_weight_change.weight, minimum_eligible_weight);
-        EFFECTIVE_USER_WEIGHTS.save(deps.storage, user, &effective_user_weight)?;
+        let old_user_weight = old_user_weight.unwrap_or_default();
 
-        let old_user_effective_weight = old_user_effective_weight.unwrap_or_default();
-
-        effective_total_weight =
-            effective_total_weight - old_user_effective_weight + effective_user_weight;
+        total_weight = total_weight - old_user_weight + new_user_weight;
     }
 
-    EFFECTIVE_TOTAL_WEIGHT.save(deps.storage, &effective_total_weight)?;
+    weights_repository_mut(deps.branch()).set_total_weight(total_weight)?;
 
-    Ok(execute_update_user_weights_response())
+    Ok(())
+}
+
+/// Called for users that we did not encounter previously.
+///
+/// Will initialize all their rewards for assets with existing distributions to 0, and set
+/// their rewards indices to current global index for each asset.
+fn initialize_user_indices(deps: DepsMut, user: Addr) -> DistributorResult<()> {
+    let all_global_indices =
+        asset_distribution_repository(deps.as_ref()).get_all_global_indices()?;
+
+    user_distribution_repository_mut(deps)
+        .initialize_distribution_info(all_global_indices, user)?;
+
+    Ok(())
+}
+
+/// Updates user's reward indices for all assets.
+///
+/// Will calculate newly pending rewards since the last update to the user's reward index until now,
+/// using their last weight to calculate the newly accrued rewards.
+fn update_user_indices(
+    deps: DepsMut,
+    user: Addr,
+    old_user_weight: Uint128,
+) -> DistributorResult<()> {
+    let all_global_indices =
+        asset_distribution_repository(deps.as_ref()).get_all_global_indices()?;
+
+    user_distribution_repository_mut(deps).update_user_indices(
+        user,
+        all_global_indices,
+        old_user_weight,
+    )?;
+
+    Ok(())
 }
 
 /// Calculate user's effective rewards weight, given their actual weight and minimum weight for
@@ -136,56 +159,4 @@ pub fn calculate_effective_weight(weight: Uint128, minimum_eligible_weight: Uint
     } else {
         Uint128::zero()
     }
-}
-
-/// Called for users that we did not encounter previously.
-///
-/// Will initialize all their rewards for assets with existing distributions to 0, and set
-/// their rewards indices to current global index for each asset.
-fn initialize_user_indices(deps: DepsMut, user: Addr) -> DistributorResult<()> {
-    let native_global_indices = NATIVE_GLOBAL_INDICES
-        .range(deps.storage, None, None, Ascending)
-        .collect::<StdResult<Vec<(String, Decimal)>>>()?;
-
-    for (denom, global_index) in native_global_indices {
-        NATIVE_DISTRIBUTIONS().update(
-            deps.storage,
-            (user.clone(), denom.clone()),
-            |distribution| -> StdResult<NativeDistribution> {
-                match distribution {
-                    None => Ok(NativeDistribution {
-                        user: user.clone(),
-                        denom,
-                        user_index: global_index,
-                        pending_rewards: Uint128::zero(),
-                    }),
-                    Some(distribution) => Ok(distribution),
-                }
-            },
-        )?;
-    }
-
-    let cw20_global_indices = CW20_GLOBAL_INDICES
-        .range(deps.storage, None, None, Ascending)
-        .collect::<StdResult<Vec<(Addr, Decimal)>>>()?;
-
-    for (asset, global_index) in cw20_global_indices {
-        CW20_DISTRIBUTIONS().update(
-            deps.storage,
-            (user.clone(), asset.clone()),
-            |distribution| -> StdResult<Cw20Distribution> {
-                match distribution {
-                    None => Ok(Cw20Distribution {
-                        user: user.clone(),
-                        cw20_asset: asset,
-                        user_index: global_index,
-                        pending_rewards: Uint128::zero(),
-                    }),
-                    Some(distribution) => Ok(distribution),
-                }
-            },
-        )?;
-    }
-
-    Ok(())
 }
